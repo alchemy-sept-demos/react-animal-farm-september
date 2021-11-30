f49e96c3b5191d28600225f5125782da808a71b8
Julie Nisbet <jmnisbet@gmail.com>
2021-11-30 15:11:30 -0800

    class code from demo

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,10 @@
-import React from 'react'
-
-export default function Animal() {
-  return <></>
+import './Animal.css'
+export default function Animal({ name, left, top, type, says }) {
+  return (
+    <div className="animal" style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  )
 }
